af8e6b0c9f90692df2094c18562a5e88b11a3930
Yash <yash.saxena1217@gmail.com>
2014-11-23 03:37:37 -0800

    address api update to https on production

--- www/js/controllers/globalController.js
+++ www/js/controllers/globalController.js
@@ -9,8 +9,16 @@ pinpoint.controller('globalController', function(
   'use strict';
 
   console.log('#### Global Controller');
+
+  // $rootScope.environment = 'Development';
+  // $rootScope.apiUrl = 'localhost:5000';
+  // $rootScope.geoApi = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=';
+  // console.log('#### Environment set: ');
+
   $rootScope.environment = 'Production';
   $rootScope.apiUrl = 'https://desolate-meadow-6374.herokuapp.com';
+  $rootScope.geoApi = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=';
   console.log('#### Environment set: ');
+
   console.log($rootScope);
 });
